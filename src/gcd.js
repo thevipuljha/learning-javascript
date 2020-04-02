e50239eafa2c0d7e20c507199236d3ac675428ba
Vipul Jha <vipuljha1142@gmail.com>
2020-04-03 02:05:33 +0530

    GCD and LCM

--- src/gcd.js
+++ src/gcd.js
@@ -1,7 +1,6 @@
 function gcd(num1,num2){
-    if(num2==0){
+    if(num2==0)
         return num1;
-    }
     return gcd(num2,num1 % num2);
 }
 let num1 = +process.argv[2];
--- src/lcm.js
+++ src/lcm.js
@@ -1,7 +1,6 @@
 function gcd(num1,num2){
-    if(num2==0){
+    if(num2==0)
         return num1;
-    }
     return gcd(num2,num1 % num2);
 }
 
