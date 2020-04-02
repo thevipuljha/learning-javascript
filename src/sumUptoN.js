b338c072b1515c6e1bb9d0599977b3cad0298b3a
Vipul Jha <vipuljha1142@gmail.com>
2020-04-03 02:03:05 +0530

    Sum of numbers from 1 to n

---
+++ src/sumUptoN.js
@@ -0,0 +1,11 @@
+let number = +process.argv[2];
+if (number<=0)
+	console.log("Natural Number Expected!");
+else {
+	let sum=0;
+	while(number>=1){
+		sum=sum+number;
+		number--;
+	}
+	console.log("Sum =",sum);
+}
