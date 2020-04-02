255d34e7c8552a0ef54383e772a584c5c48bad3d
Vipul Jha <vipuljha1142@gmail.com>
2020-04-03 02:02:01 +0530

    Sum of numbers from n to m(even/odd)

---
+++ src/sumOddNToM.js
@@ -0,0 +1,11 @@
+let first= +process.argv[2];
+let last= +process.argv[3];
+let sum=0;
+if (first>last)
+	console.log("First Number must be smaller");
+else{
+	for(let i=first;i<=last;i++)
+		if (i%2!=0)
+			sum+=i;
+	console.log("Sum = ",sum);
+}
