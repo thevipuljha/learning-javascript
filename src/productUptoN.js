d48f05f1c49f2021a889000c3c570faea889c485
Vipul Jha <vipuljha1142@gmail.com>
2020-04-08 13:34:56 +0530

    product of all number from 1 to N

--- src/productUptoN.js
+++ src/productUptoN.js
@@ -1,11 +1,11 @@
 let number = +process.argv[2];
-if (number<=0)
-	console.log("Natural Number Expected");
-else {
-	let pro=1;
-	while(number>=1){
-    	pro=pro*number;
-    	number--;
-	}       
-	console.log("Product =",pro);
+let product = 1;
+if (number > 0) {
+	while (number >= 1) {
+		product = product * number;
+		number--;
+	}
+	console.log("Product =", product);
 }
+else
+	console.log("Natural Number expected")
