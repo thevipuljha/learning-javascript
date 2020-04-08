8a60e82d60e825566ace7a0b14e54aa2971ea645
Vipul Jha <vipuljha1142@gmail.com>
2020-04-08 10:22:47 +0530

    print 0 also now

--- src/print_evens.js
+++ src/print_evens.js
@@ -1,9 +1,9 @@
 let input = +process.argv[2];
 if (input < 0) {
-	for (let count = -2; count >= input; count = count - 2)
+	for (let count = 0; count >= input; count = count - 2)
 		console.log(count);
 }
 else {
-	for (let count = 2; count <= input; count = count + 2)
+	for (let count = 0; count <= input; count = count + 2)
 		console.log(count);
 }
