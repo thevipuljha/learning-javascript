51b2123e5b5ceea08a4ccc13a196913cdbea84e9
Vipul Jha <vipuljha1142@gmail.com>
2020-04-08 11:09:52 +0530

    print all even/odd backwards between 2 Numbers

--- src/printBackOdd.js
+++ src/printBackOdd.js
@@ -1,8 +1,8 @@
-let first= +process.argv[2];
-let last= +process.argv[3];
-if (first<last)
+let first = +process.argv[2];
+let last = +process.argv[3];
+if (first < last)
 	console.log("Second input must be smaller");
 else
-	for(let i=first;i>=last;i--)
-		if (i%2!=0)
-			console.log(i);
+	for (let count = first; count >= last; count--)
+		if (count % 2 != 0)
+			console.log(count);
