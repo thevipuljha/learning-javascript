c2c333f5d3ea1914f9e1e0114c270bc496475f43
Vipul Jha <vipuljha1142@gmail.com>
2020-04-08 09:21:14 +0530

    GCD of two numbers

--- src/gcd.js
+++ src/gcd.js
@@ -1,9 +1,9 @@
-function gcd(num1,num2){
-    if(num2==0)
+function gcd(num1, num2) {
+    if (num2 == 0)
         return num1;
-    return gcd(num2,num1 % num2);
+    return gcd(num2, num1 % num2);
 }
-let num1 = +process.argv[2];
-let num2 = +process.argv[3];
+let number1 = +process.argv[2];
+let number2 = +process.argv[3];
 
-console.log(`GCD of ${num1} and ${num2} is ${gcd(num1,num2)}`);
+console.log(`GCD of ${number1} , ${number2} = ${gcd(number1, number2)}`);
