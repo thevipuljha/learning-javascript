b89f6459a5d9e42bf0d67589fa2adc7fa8679b0c
Vipul Jha <vipuljha1142@gmail.com>
2020-04-08 09:33:33 +0530

    LCM of two numbers

--- src/lcm.js
+++ src/lcm.js
@@ -1,10 +1,10 @@
-function gcd(num1,num2){
-    if(num2==0)
+function gcd(num1, num2) {
+    if (num2 == 0)
         return num1;
-    return gcd(num2,num1 % num2);
+    return gcd(num2, num1 % num2);
 }
 
-let num1 = +process.argv[2];
-let num2 = +process.argv[3];
-let lcm = (num1*num2)/gcd(num1,num2);
-console.log(`LCM of ${num1} and ${num2} is ${lcm}`);
+let number1 = +process.argv[2];
+let number2 = +process.argv[3];
+let lcm = (number1 * number2) / gcd(number1, number2);
+console.log(`LCM of ${number1} , ${number2} = ${lcm}`);
