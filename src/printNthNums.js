2cdd2174520a736d7c6661aa6f9977eef2c3dd1f
Vipul Jha <vipuljha1142@gmail.com>
2020-04-08 11:22:51 +0530

    better code 1

--- src/printNthNums.js
+++ src/printNthNums.js
@@ -1,8 +1,7 @@
-let first= +process.argv[2];
-let last= +process.argv[3];
-let diff= +process.argv[4];
-if (first>last)
-	console.log("First Number must be smaller than second");
-else
-	for(let i=first;i<=last;i=i+diff)
-			console.log(i);
+//Print every Nth number from P to Q.
+//Input syntax "P Q N"
+let first = +process.argv[2];
+let last = +process.argv[3];
+let gap = +process.argv[4];
+for (let count = first; count <= last; count = count + gap)
+	console.log(count);
